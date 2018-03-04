66d035be6078c9b544247e80364619aee49ec08f
surarin <surarin_ex@icloud.com>
2018-03-04 22:10:01 +0000

    ログから日付を除去

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const jade = require('jade');
 const server = http.createServer((req, res) => {
   const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+  console.info('Requested by ' + req.connection.remoteAddress);
   res.writeHead(200, {
     'Content-Type': 'text/html; charset=utf-8'
   });
@@ -38,7 +38,7 @@ const server = http.createServer((req, res) => {
       }).on('end', () => {
         body = Buffer.concat(body).toString();
         const decoded = decodeURIComponent(body);
-        console.info('[' + now + '] 投稿: ' + decoded);
+        console.info('投稿: ' + decoded);
         res.write('<!DOCTYPE html><html lang="ja"><head><meta charset="utf-8"></head><body><h1>' +
           decoded + 'が投稿されました</h1></body></html>');
         res.end();
